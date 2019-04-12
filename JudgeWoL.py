6f4fc10e1b70a24ea6ece6a123f26f4fa106ae25
trigy <89495755@qq.com>
2019-04-12 14:44:39 +0800

    改了改，需要把两个文件弄在一起
    正在弄

--- JudgeWoL.py
+++ JudgeWoL.py
@@ -15,14 +15,13 @@ judge winer:
     0  : not end
     -1  : white win
 """
-count = 0
+con_count = 0   #the count of continuously piece 
 check_map = []
-
 def black_DFS(x,y):
-    global count
+    global con_count
     global check_map
     check_map[x][y] == 10
-    count += 1
+    con_count += 1
     if check_map[x-1][y-1] == 1:
         black_DFS(x-1,y-1)    
     if check_map[x - 1][y] == 1:
@@ -41,10 +40,10 @@ def black_DFS(x,y):
         black_DFS(x+1,y+1)   
         
 def white_DFS(x,y):
-    global count
+    global con_count
     global check_map
     check_map[x][y] == 20
-    count += 1
+    con_count += 1
     if check_map[x-1][y-1] == 2:
         white_DFS(x-1,y-1)    
     if check_map[x - 1][y] == 2:
@@ -65,37 +64,44 @@ def white_DFS(x,y):
 def judgeWin():
     global black_piece
     global white_piece
-    global count
+    global black_piece_count
+    global white_piece_count
+    global con_count
     global check_map
     global board_situation
-    black_count = len(black_piece)
-    white_count = len(white_piece)
     check_map = board_situation
     #expand the size to avoid out of index
     for i in range(0,9):
         check_map[i].append(-1)
     check_map.append([-1,-1,-1,-1,-1,-1,-1,-1,-1,-1])
-    if black_count == 1:
-        return -1
-    if white_count == 1:
+    if white_piece_count == 1:
         return 1
+    if black_piece_count == 1:
+        return -1
+    
     
     #check black
-    count = 0
-    check_x = black_piece[0][0]
-    check_y = black_piece[0][1]
+    con_count = 0
+    for i in range(0,9):
+        if black_piece[i][0] != 114:
+            break
+    check_x = black_piece[i][0]
+    check_y = black_piece[i][1]
     #for i in range(0,len(black_piece)):
     #    check_list.append(0)   #0 represent not check
     black_DFS(check_x,check_y)
-    if count == black_count:
+    if con_count == black_piece_count:
         return 1
 
     #check white
-    count = 0
-    check_x = white_piece[0][0]
-    check_y = white_piece[0][1]
+    con_count = 0
+    for i in range(0,9):
+        if white_piece[i][0] != 114:
+            break
+    check_x = white_piece[i][0]
+    check_y = white_piece[i][1]
     white_DFS(check_x,check_y)
-    if count == white_count:
+    if con_count == white_piece_count:
         return -1
     
     return 0
